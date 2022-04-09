717579394ddbfa95bbd77a0142f3933461cd6ed6
Aaron Piotrowski <aaron@trowski.com>
2022-04-09 12:14:20 -0500

    Forgot to mark as internal

--- src/Internal/FieldMessage.php
+++ src/Internal/FieldMessage.php
@@ -4,6 +4,7 @@ namespace Amp\Http\Server\FormParser\Internal;
 
 use Amp\Http\Message;
 
+/** @internal */
 final class FieldMessage extends Message
 {
     public function __construct(array $headers)
